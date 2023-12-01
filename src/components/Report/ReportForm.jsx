ce526503d8d453a7de6dac4e8af96cecc74f2821
Felipe <felipecassianobarbosa@hotmail.com>
2023-12-01 16:41:29 -0300

    refactor: handler realocation

--- src/components/Report/ReportForm.jsx
+++ src/components/Report/ReportForm.jsx
@@ -1,50 +1,11 @@
 import Container from "react-bootstrap/esm/Container";
 import Button from 'react-bootstrap/Button'
-import { api } from "../../services/api";
-import { useNavigate } from "react-router-dom";
 import '../../styles/ReportForm.css';
 
-function ReportForm() {
-    const nav = useNavigate()
-    const user = JSON.parse(window.localStorage.getItem('user'));
+function ReportForm({handler}) {
     const pets = JSON.parse(window.localStorage.getItem('pets'));
-    function handlePost() {
-        const date = new Date(document.getElementById('date').value);
-        const cost = document.getElementById('cost').value.replace(',', '.');
-        const brand = document.getElementById('brand').value;
-        const amount = parseInt(document.getElementById('amount').value);
-        const petId = parseInt(document.getElementById('pet').options[document.getElementById('pet').options.selectedIndex].value);
-
-        const allowance = handleVerify(!isNaN(date), 'Data inválida.') &&
-            handleVerify(!(date.getFullYear() < 2010), 'Data antiga demais.') &&
-            handleVerify(!(date.getTime() > new Date(Date('now')).getTime()), 'Datas futuras não podem ser inseridas.') &&
-            handleVerify(cost != '', 'Custo inválido.') &&
-            handleVerify(!isNaN(parseFloat(cost.replace('.', ','))), 'Custo inválido.') &&
-            handleVerify(brand.length < 18, 'Nome de marca longo demais.') &&
-            handleVerify(brand.length > 0, 'Nome de marca inválido.') &&
-            handleVerify(brand.replace(' ', '').replace(' ', '').length > 0, 'Nome de marca inválido.') &&
-            handleVerify(amount > 0, 'Quantidade inválida.') &&
-            handleVerify(petId, 'Pet inválido.');
-        if (allowance) {
-            api.post(`/reports?id=${user.id}`, {
-                date: date.toISOString(),
-                cost: cost,
-                brand: brand,
-                amount: amount,
-                userId: user.id,
-                petId: petId
-            }).then(() => nav('/'));
-        }
-    }
     function submit(e) {
         e.preventDefault();
-    }
-    function handleVerify(statement, message) {
-        if (statement) {
-            return true;
-        }
-        alert(message);
-        return false;
     }
     return (
         <form onSubmit={submit} method="post">
@@ -70,7 +31,7 @@ function ReportForm() {
                     {pets.map((pet) => <option key={pet.id} value={pet.id}>{pet.name}</option>)}
                 </select>
             </Container>
-            <Button onClick={handlePost} type="button" className="proj-30">Enviar</Button>
+            <Button onClick={handler} type="button" className="proj-30">Enviar</Button>
         </form>
     );
 }
--- src/pages/NewReport.jsx
+++ src/pages/NewReport.jsx
@@ -1,16 +1,69 @@
 import Container from "react-bootstrap/esm/Container";
 import ReportForm from "../components/Report/ReportForm";
-
+import Message from "../components/Fixed/Message";
+import { api } from "../services/api";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function NewReport() {
     document.title = 'Meu Zeus | Novo relatório'
+    const user = JSON.parse(window.localStorage.getItem('user'));
+    const nav = useNavigate();
+    const [show, setShow] = useState(false);
+    const [message, setMessage] = useState('');
+    const [type, setType] = useState('');
+    function handlePost() {
+        const date = new Date(document.getElementById('date').value);
+        const cost = document.getElementById('cost').value;
+        const brand = document.getElementById('brand').value;
+        const amount = parseInt(document.getElementById('amount').value.replace(',', '').replace('.', ''));
+        const petId = parseInt(document.getElementById('pet').options[document.getElementById('pet').options.selectedIndex].value);
+        const allowance = handleVerify(!isNaN(date), 'Data inválida.') &&
+            handleVerify(!(date.getFullYear() < 2010), 'Data antiga demais.') &&
+            handleVerify(!(date.getTime() > new Date(Date('now')).getTime()), 'Datas futuras não podem ser inseridas.') &&
+            handleVerify(cost != '', 'Custo inválido.') &&
+            handleVerify(!isNaN(parseFloat(cost.replace('.', ','))), 'Custo inválido.') &&
+            handleVerify(brand.length < 18, 'Nome de marca longo demais.') &&
+            handleVerify(brand.length > 0, 'Nome de marca inválido.') &&
+            handleVerify(brand.replace(' ', '').replace(' ', '').length > 0, 'Nome de marca inválido.') &&
+            handleVerify(amount > 0, 'Quantidade inválida.') &&
+            handleVerify(petId, 'Pet inválido.');
+            
+        if (allowance) {
+            api.post(`/reports?id=${user.id}`, {
+                date: date.toISOString(),
+                cost: cost,
+                brand: brand,
+                amount: amount,
+                userId: user.id,
+                petId: petId
+            }).then((response) => {
+                setMessage(response.data.message);
+                setType(response.data.type);
+                setShow(true);
+                if (response.data.type === 'success') {
+                    setTimeout(() => {
+                        nav('/')
+                    }, 3000);
+                }
+            });
+        }
+    }
+    function handleVerify(statement, message) {
+        if (statement) {
+            return true;
+        }
+        alert(message);
+        return false;
+    }
     return (
         <>
             <Container className="my-5">
                 <Container>
                     <h1>Adicionar relatório</h1>
                 </Container>
-                <ReportForm/>
+                <Message show={show} txt={message} type={type} />
+                <ReportForm handler={handlePost} />
             </Container>
         </>
     );

656ff6c2f8dc6c22227559e3c3a556f955cb25a2
Felipe <felipecassianobarbosa@hotmail.com>
2023-12-05 16:02:04 -0300

    feat: use user context, pet deletion

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,62 +2,80 @@ import { Button, Container, Image } from "react-bootstrap";
 import placeholder from '../assets/placeholder.jpeg';
 import { useNavigate } from "react-router-dom";
 import PasswordModal from "../components/User/PasswordModal";
+import PetModal from "../components/Pet/PetModal";
 import PetCarousel from "../components/Pet/PetCarousel";
 import { useState } from "react";
 import { api } from "../services/api";
 import '../styles/Profile.css'
+import { useAuth } from "../context/useAuth";
+import Message from '../components/Fixed/Message'
 
 function Profile() {
-    const user = JSON.parse(window.localStorage.getItem('user'));
-    const pets = JSON.parse(window.localStorage.getItem('pets'));
+    const { loggedUser, logout, pets } = useAuth();
     document.title = 'Meu Zeus | Perfil';
-    const [show, setShow] = useState(false);
-    const nav = useNavigate();
+    const [showPasswordModal, setShowPasswordModal] = useState(false);
+    const [showPetModal, setShowPetModal] = useState(false);
+    const [showMessage, setShowMessage] = useState(false);
+    const [message, setMessage] = useState('');
+    const [type, setType] = useState('');
     function updatePassword() {
-        handleShow();
+        handleClose();
         const currentPassword = document.getElementById('currentPassword').value;
         const newPassword = document.getElementById('newPassword').value;
-        api.put(`/users/${user.id}`, {
+        api.put(`/users/${loggedUser.id}`, {
             current: currentPassword,
             new: newPassword
-        }).then((response) => window.localStorage.setItem('user', response.data))
+        }).then((response) => {
+            setMessage(response.data.message);
+            setType(response.data.type);
+            setShowMessage(true);
+            setTimeout(() => {
+                setShowMessage(false);
+            }, 3000);
+        })
     }
     function handleLogout() {
-        window.localStorage.removeItem('user');
-        window.localStorage.removeItem('reports');
-        nav('/');
-        window.location.reload();
+        logout();
     }
-    function handleClose() {
-        setShow(false)
+    function handleClosePasswordModal() {
+        setShowPasswordModal(false)
     }
-    function handleShow() {
-        setShow(true);
+    function handleShowPasswordModal() {
+        setShowPasswordModal(true);
+    }
+    function handleClosePetModal() {
+        setShowPetModal(false)
+    }
+    function handleShowPetModal() {
+        setShowPetModal(true);
     }
     return (
         <Container className="profile">
-            <PasswordModal user={user} show={show} handleClose={handleClose} updatePassword={updatePassword} />
-            <h1>Perfil de {user.username}</h1>
+            <PasswordModal user={loggedUser} show={showPasswordModal} handleClose={handleClosePasswordModal} updatePassword={updatePassword} />
+            <PetModal show={showPetModal} handleClose={handleClosePetModal} />
+            <h1>Perfil de {loggedUser.username}</h1>
             <hr />
+            <Message show={showMessage} txt={message} type={type} />
             <Container className="d-flex justify-content-between">
-                <Image width={256} height={256} className="align-self-center profile-img" src={placeholder} />
+                <Image className="align-self-center profile-img" src={placeholder} />
                 <Container className="profile-info">
                     <Container>
                         <h4 className="fw-bold">Informações pessoais</h4>
                         <h6 className="fw-bold">Usuário: </h6>
-                        <p>{user.username}</p>
+                        <p>{loggedUser.username}</p>
                         <h6 className="fw-bold">Senha</h6>
                         <p>••••••••</p>
                     </Container>
                     <Container className="profile-options">
-                        <a onClick={handleShow}>Alterar senha</a>
+                        <a onClick={handleShowPasswordModal}>Alterar senha</a>
                         <a className="text-danger text-decoration-none" onClick={handleLogout}>Sair</a>
                     </Container>
                 </Container>
                 <Container className="profile-pet">
                     <h4 className="fw-bold">Seus pets</h4>
                     <PetCarousel pets={pets} />
-                    <Button href="/newpet" className="proj-30">Adicionar novo pet</Button>
+                    <Button href="/newpet" className=" mt-1 proj-30">Adicionar novo pet</Button>
+                    <Button onClick={handleShowPetModal} className=" mt-1 fw-bold" variant="danger">Remover pet</Button>
                 </Container>
             </Container>
         </Container>

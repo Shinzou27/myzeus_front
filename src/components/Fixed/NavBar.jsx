e03d29688461b9dbc610e7271eb547de2fa9e2ae
Felipe <felipecassianobarbosa@hotmail.com>
2023-12-05 16:05:47 -0300

    fix: bad style on different screen sizes

--- src/components/Fixed/NavBar.jsx
+++ src/components/Fixed/NavBar.jsx
@@ -5,14 +5,13 @@ import Navbar from 'react-bootstrap/Navbar';
 import Image from 'react-bootstrap/Image'
 import { useNavigate } from 'react-router-dom';
 import '../../styles/Navbar.css';
+import { useAuth } from '../../context/useAuth';
 
 function NavBar() {
-    const user = JSON.parse(window.localStorage.getItem('user'));
+    const { loggedUser, logout } = useAuth();
     const nav = useNavigate();
     function pseudoMiddleware(e) {
-        window.localStorage.removeItem('user');
-        window.localStorage.removeItem('reports');
-        window.localStorage.removeItem('pets');
+        logout();
         nav('/');
         window.location.reload();
     }
@@ -20,26 +19,29 @@ function NavBar() {
         <Navbar fixed='top' bg='dark' expand='lg'>
             <Container>
                 <Navbar.Brand href='/'><Image src={logo} height={16}></Image></Navbar.Brand>
-                <Nav className='me-auto fs-5'>
-                    <Nav.Link href='/' className='text-light nav-text'>Início</Nav.Link>
-                    {user && <>
-                        <Nav.Link href='/overview' className='text-light nav-text'>Visão Geral</Nav.Link>
-                        <Nav.Link href='/dashboard' className='text-light nav-text'>Painel de Controle</Nav.Link>
-                    </>
-                    }
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className='me-auto fs-5'>
+                        <Nav.Link href='/' className='text-light nav-text'>Início</Nav.Link>
+                        {loggedUser.username && <>
+                            <Nav.Link href='/overview' className='text-light nav-text'>Visão Geral</Nav.Link>
+                            <Nav.Link href='/dashboard' className='text-light nav-text'>Painel de Controle</Nav.Link>
+                        </>
+                        }
 
-                </Nav>
-                <Nav>
-                    {user ?
-                        <>
-                            <Nav.Link href='/profile' className='text-light nav-text ms-auto fw-bold'>{user.username}</Nav.Link>
-                            <Nav.Link onClick={pseudoMiddleware} className='text-light nav-text ms-auto' >Sair</Nav.Link>
-                        </> :
-                        <>
-                            <Nav.Link href='/login' className='text-light nav-text ms-auto' >Entrar</Nav.Link>
-                            <Nav.Link href='/register' className='text-light nav-text ms-auto'>Criar Conta</Nav.Link>
-                        </>}
-                </Nav>
+                    </Nav>
+                    <Nav>
+                        {loggedUser.username ?
+                            <>
+                                <Nav.Link href='/profile' className='text-light nav-text ms-auto fw-bold'>{loggedUser.username}</Nav.Link>
+                                <Nav.Link onClick={pseudoMiddleware} className='text-light nav-text ms-auto' >Sair</Nav.Link>
+                            </> :
+                            <>
+                                <Nav.Link href='/login' className='text-light nav-text ms-auto' >Entrar</Nav.Link>
+                                <Nav.Link href='/register' className='text-light nav-text ms-auto'>Criar Conta</Nav.Link>
+                            </>}
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     )
--- src/components/Home/Feedback.jsx
+++ src/components/Home/Feedback.jsx
@@ -1,4 +1,4 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { Card } from "react-bootstrap";
 import { Image } from "react-bootstrap";
 import app_icon from '../../assets/app_icon.png'
@@ -6,30 +6,35 @@ import app_icon from '../../assets/app_icon.png'
 function Feedback() {
     return (
         <>
-            <Container className="d-flex my-5 py-5">
-                <Container className="align-self-center feedback-left">
+            <Row className="d-flex my-5 py-5">
+                <Col md={3} className="align-self-center feedback-left">
                     <h3>Conheça alguns de nossos clientes!</h3>
                     <hr />
                     <p>Veja o que outras pessoas acham do Meu Zeus.</p>
-                </Container>
-                <Container className="d-flex">
-                    <Card className="mx-2 pet-card">
+                </Col>
+                <Col md={3}>
+                    <Card className="pet-card">
                         <Card.Title className="pet-name">Zeus (Bulldog)</Card.Title>
                         <Card.Body className="feedback-desc">"A plataforma é definitivamente uma das plataformas existentes. Depois de usá-la para gerenciar as rações que comprava, continuei sendo o mesmo!"</Card.Body>
                         <Card.Footer className="pet-owner">Fulano I, São Paulo - SP</Card.Footer>
                     </Card>
-                    <Card className="mx-2 pet-card">
+                </Col>
+                <Col md={3}>
+                    <Card className="pet-card">
                         <Card.Title className="pet-name">Zeus (Gato)</Card.Title>
                         <Card.Body className="feedback-desc">"O Meu Zeus me ajudou muito! Agora, continuo comprando as mesmas rações, porém sei quanto sai do meu bolso todo mês."</Card.Body>
                         <Card.Footer className="pet-owner">Fulano II, Niterói - RJ</Card.Footer>
                     </Card>
-                    <Card className="mx-2 pet-card">
+                </Col>
+                <Col md={3}>
+                    <Card className="pet-card">
                         <Card.Title className="pet-name">Zeus (Pastor Alemão)</Card.Title>
                         <Card.Body className="feedback-desc">"Acabou a criatividade. Não tenho mais o que escrever. Só tô aqui porque preciso ser um placeholder um pouco maior!"</Card.Body>
                         <Card.Footer className="pet-owner">Fulano III, Caucaia - CE</Card.Footer>
                     </Card>
-                </Container>
-            </Container>
+                </Col>
+            </Row>
+            <hr />
             <Container className="app-container w-75">
                 <h2>Baixe também nosso <span>app</span>!</h2>
                 <Container className="d-flex w-75">

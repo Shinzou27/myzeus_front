ef69c5a93ca9080527afebdb0426abf3fbd52120
Felipe <felipecassianobarbosa@hotmail.com>
2023-12-05 15:58:58 -0300

    refactor: better style on filter buttons

--- src/components/Table/TableFilter.jsx
+++ src/components/Table/TableFilter.jsx
@@ -2,16 +2,18 @@ import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/esm/Row';
 import Col from 'react-bootstrap/esm/Col';
 import Container from 'react-bootstrap/esm/Container';
+import { useAuth } from '../../context/useAuth';
+
 function TableFilter({ handleClick, handlePet }) {
-    const pets = JSON.parse(window.localStorage.getItem('pets'));
+    const {pets} = useAuth();
     function sendDates() {
         const start = document.getElementById('start').value;
         const end = document.getElementById('end').value;
         handleClick(start, end);
     }
     function sendPets(e, id) {
-        const proj10 = 'pet-button proj-10 col';
-        const proj30 = 'pet-button proj-30 col';
+        const proj10 = 'pet-button proj-10 col selected';
+        const proj30 = 'pet-button proj-30 col unselected';
         e.target.className == proj10 ? e.target.className = proj30 : e.target.className = proj10;
         handlePet(id);
     }
@@ -30,7 +32,7 @@ function TableFilter({ handleClick, handlePet }) {
             <h6 className='my-2'>Filtrar por pet</h6>
             <Row  id='filter-container'>
                 {pets.map((pet) => (
-                    <Col className='pet-button proj-30' key={pet.id} onClick={(e) => sendPets(e, pet.id)}>{pet.name}</Col>
+                    <Col className='pet-button proj-30 unselected' key={pet.id} onClick={(e) => sendPets(e, pet.id)}>{pet.name}</Col>
                 ))}
             </Row>
             <Button onClick={sendDates} className='my-2 proj-30 filter-button'>Filtrar</Button>
